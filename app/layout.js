6f5a9762dca0516f4b8be14b74e97d6d33127abf
msak-ai <kayvon.m.adderley@gmail.com>
2024-10-08 14:09:49 -0400

    StatCounter

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <script type="text/javascript">
+          var sc_project=13006461;
+          var sc_invisible=1;
+          var sc_security="3ab8d0c3";
+        </script>
+        <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+        <noscript><div class="statcounter">
+          <a title="Web Analytics" href="https://statcounter.com/" target="_blank">
+            <img class="statcounter" src="https://c.statcounter.com/13006461/0/3ab8d0c3/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade" />
+          </a></div></noscript>
+        {children}
+      </body>
+
     </html>
   );
 }
